60ad6d4be045d3320d157862439b4fbd9ca7b1c8
UchuNoKen <xen.o.fite@gmail.com>
2019-05-02 15:27:36 -0400

    finished stripe animations

--- src/Components/Home/featured/Stripes.js
+++ src/Components/Home/featured/Stripes.js
@@ -6,7 +6,7 @@ class Stripes extends Component {
   state = {
     stripes: [
       { background: "#98c5e9", left: 120, rotate: 25, top: -260, delay: 0 },
-      { background: "#ffffff", left: 360, rotate: 25, top: -2, delay: 200 },
+      { background: "#ffffff", left: 360, rotate: 25, top: -397, delay: 200 },
       { background: "#98c5e9", left: 600, rotate: 25, top: -498, delay: 400 }
     ]
   };
@@ -17,15 +17,32 @@ class Stripes extends Component {
         key={i}
         show={true}
         start={{
-          background: "#ffffff"
+          background: "#ffffff",
+          opacity: 0,
+          left: 0,
+          rotate: 0,
+          top: 0
         }}
         enter={{
           background: [stripe.background],
-          timing: { delay: 500, duration: 200, ease: easePolyOut }
+          opacity: [1],
+          left: [stripe.left],
+          rotate: [stripe.rotate],
+          top: [stripe.top],
+          timing: { delay: stripe.delay, duration: 200, ease: easePolyOut }
         }}
       >
-        {({ background }) => {
-          return <div className="stripe" style={{ background }} />;
+        {({ opacity, left, rotate, top, background }) => {
+          return (
+            <div
+              className="stripe"
+              style={{
+                background,
+                opacity,
+                transform: `rotate(${rotate}deg) translate(${left}px, ${top}px)`
+              }}
+            />
+          );
         }}
       </Animate>
     ));
