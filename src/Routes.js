b0f8b4d5c8afe962541f97167e88ea6705b4df2e
UchuNoKen <xen.o.fite@gmail.com>
2019-05-02 11:05:41 -0400

    routes setup

---
+++ src/Routes.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Routes = props => {
+  return <div>Hello</div>;
+};
+
+export default Routes;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./Resources/css/app.css";
 
-import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import Routes from "./Routes";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById("root"));
