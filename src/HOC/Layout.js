7d72ef47354d7897c18fd2dcbeaef77931c34d4a
UchuNoKen <xen.o.fite@gmail.com>
2019-05-02 11:38:05 -0400

    built the header, added Links

--- src/HOC/Layout.js
+++ src/HOC/Layout.js
@@ -1,7 +1,13 @@
 import React from "react";
+import Header from "../Components/Header_Footer/Header";
 
 const Layout = props => {
-  return <div>Hello Layout{props.children}</div>;
+  return (
+    <div>
+      <Header />
+      {props.children}
+    </div>
+  );
 };
 
 export default Layout;
